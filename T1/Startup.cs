5416718bd60aa2681d29495081c923758a5f2888
penn song <pennsong07@gmail.com>
2013-12-16 20:45:51 +0800

    git@github.com:pennsong/T1.git

---
+++ T1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T1.Startup))]
+namespace T1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
